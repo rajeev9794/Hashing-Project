087b91165793df68eda8528c5ed98df8811a914b
RAJEEV MAURYA <43512770+rajeev9794@users.noreply.github.com>
2019-07-08 09:51:49 +0530

    Add files via upload

---
+++ filesingly.h
@@ -0,0 +1,88 @@
+#include<iostream>
+//#include"Record.h"
+using namespace std;
+template<class T>
+class node
+{
+public:
+	T info;
+	node<T> *next;
+
+	node()
+	{
+		info=0;
+		next=NULL;
+	}
+	node(T x)
+	{
+		info=x;
+		next=NULL;
+	}
+	~node()
+	{
+	}
+	
+	
+};
+template<class T>
+class sllist
+{
+	node<T> *head;
+public:
+	sllist()
+	{
+		head=NULL;
+	}
+	~sllist()
+	{
+	
+	}
+	void insbeg(T x);
+	void inslast(T x);
+	void display_sll();
+};
+template<class T>
+void sllist<T>::insbeg(T x)
+{	
+	node<T> *p;
+	p=new node<T>(x);
+	if(head==NULL)
+	{
+		head=p;
+	}
+	else{
+	p->next=head;
+	head=p;
+	}
+}
+template<class T>
+void sllist<T>::inslast(T x)
+{
+	node<T> *p,*q,*r;
+	q=new node<T>(x);
+	p=head;
+	if(p==NULL)
+	{
+	head=q;
+	}
+	else{
+	
+	while(p!=NULL)
+	{
+	r=p;
+	p=p->next;
+	}
+	r->next=q;
+	}
+}
+template<class T>
+void sllist<T>::display_sll()
+{
+	node<T> *p;
+	p=head;
+	while(p!=NULL)
+	{
+		cout<<"->"<<p->info;
+		p=p->next;
+	}
+}
